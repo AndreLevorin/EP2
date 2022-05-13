4d0a0bf8988fd89fdaf47e0c533bb37b7c9d17e6
AndreLevorin <105468771+AndreLevorin@users.noreply.github.com>
2022-05-12 23:23:03 -0300

    Update ep2.py

--- ep2.py
+++ ep2.py
@@ -1,17 +1,32 @@
 #EP2 por André Levorin e Luigi Zema
 
-# Normalizando Base de Países
+#imports
+import random
+import math
+#=========#
 
+
+
+
+# Normalizando Base de Países
 def normaliza(dados_paises):
     base_paises={}
-
     for continente in dados_paises:
         pais_continente=dados_paises[continente]
         for pais in pais_continente:
             v=pais_continente[pais]
             v['Continente']=continente
             base_paises[pais]=v
-             
     return base_paises
-  
+#======================================================#  
+    
+    
+    
+
+# Sorteando Países
+def sorteia_pais(dicionario_paises):
+    lista_chaves_paises = list(dicionario_paises.keys())
+    random_chave_paises = random.choice(lista_chaves_paises)
+    return (random_chave_paises)    
+#=============================================================#
   
