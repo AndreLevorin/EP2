2c052595c5c0209c52ab762d8134e4d3b15ea100
LuigiZema <105470377+LuigiZema@users.noreply.github.com>
2022-05-12 23:25:18 -0300

    Update ep2.py

--- ep2.py
+++ ep2.py
@@ -3,7 +3,7 @@
 #imports
 import random
 import math
-#=========#
+
 
 
 
@@ -18,15 +18,31 @@ def normaliza(dados_paises):
             v['Continente']=continente
             base_paises[pais]=v
     return base_paises
-#======================================================#  
-    
-    
-    
+
+
 
 # Sorteando Países
 def sorteia_pais(dicionario_paises):
     lista_chaves_paises = list(dicionario_paises.keys())
     random_chave_paises = random.choice(lista_chaves_paises)
     return (random_chave_paises)    
-#=============================================================#
-  
+
+
+
+# Distância de Haversine
+def haversine(raio, latitude_A, longitude_A, latitude_B, longitude_B):
+    lat_a_rad = math.radians(latitude_A)
+    lat_b_rad = math.radians(latitude_B)
+    lon_a_rad = math.radians(longitude_A)
+    lon_b_rad = math.radians(longitude_B)
+    dif_lat = (lat_b_rad - lat_a_rad ) / 2
+    dif_lon = (lon_b_rad - lon_a_rad ) / 2
+    a = math.sin(dif_lat)**2
+    b = math.sin(dif_lon)**2
+    c = math.cos(lat_a_rad) * math.cos(lat_b_rad)
+    angulo = math.sqrt(a + c * b)
+    haversine_formula = 2 * raio * math.asin(angulo)
+    return haversine_formula    
+
+
+
