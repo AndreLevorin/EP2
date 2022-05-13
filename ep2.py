da76a43d743d5d6e6e5e17f95d0ed4371d69e7a6
LuigiZema <105470377+LuigiZema@users.noreply.github.com>
2022-05-12 23:20:12 -0300

    Update ep2.py

--- ep2.py
+++ ep2.py
@@ -1,1 +1,17 @@
 #EP2 por André Levorin e Luigi Zema
+
+# Normalizando Base de Países
+
+def normaliza(dados_paises):
+    base_paises={}
+
+    for continente in dados_paises:
+        pais_continente=dados_paises[continente]
+        for pais in pais_continente:
+            v=pais_continente[pais]
+            v['Continente']=continente
+            base_paises[pais]=v
+             
+    return base_paises
+  
+  
